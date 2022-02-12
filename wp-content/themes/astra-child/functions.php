8c252fbbde50bb946441b643209dc7c27a80dab1
Bojan Lukic <bojanitlukic@gmail.com>
2022-02-12 18:34:15 +0100

    display custom field text

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -7,3 +7,14 @@
  * @package Astra Child
  * @since 1.0.0
  */
+
+add_action( 'woocommerce_single_product_summary', 'display_custom_field' );
+function display_custom_field() {
+    $customText = get_field('custom_field_text');
+
+    ?>
+    <span class="woocommerce-summary__custom-field-text">
+        <?php esc_html_e($customText); ?>
+    </span>
+    <?php
+}
