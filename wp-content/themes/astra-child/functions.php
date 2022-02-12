c6b097341e465407172dd4c09941e6d7f134c8f7
Bojan Lukic <bojanitlukic@gmail.com>
2022-02-12 18:30:14 +0100

    create child theme

---
+++ wp-content/themes/astra-child/functions.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Astra Child Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Astra Child
+ * @since 1.0.0
+ */
